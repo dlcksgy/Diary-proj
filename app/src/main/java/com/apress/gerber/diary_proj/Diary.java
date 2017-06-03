e6c4fd29722e39e4fac3542e4a40cb16fcbd4f98
dlcskgy <hyo517@naver.com>
2017-06-04 00:27:49 +0900

    Diary클래스를 만들고 노말다이어리액티비티의 자동 날짜 입력버튼과 저장버튼의 기능을 완성함.

--- app/src/main/java/com/apress/gerber/diary_proj/Diary.java
+++ app/src/main/java/com/apress/gerber/diary_proj/Diary.java
@@ -1,66 +1,86 @@
 package com.apress.gerber.diary_proj;
+import java.util.Calendar;
 
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
-
+import android.content.Context;
 /**
  * Created by Arduino on 2017-06-01.
  */
 
 public class Diary {
 
-    public static void inputDiary(String fileName, String input) {
-        File file = new File(fileName);
-        FileWriter fw = null;
 
-        try {
-            // open file.
-            fw = new FileWriter(file);
+    private Context context;
+    private int year;
+    private int month;
+    private int day;
+    private String diaryText;
 
-            // write file.
-            fw.write(input);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        // close file.
-        if (fw != null) {
-            // catch Exception here or throw.
-            try {
-                fw.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
+    public Diary(Context context,String txt){
+        this.context = context;
+        this.diaryText = txt;
     }
 
-    public static String loadDiary(String fileName){
-        File file = new File(fileName) ;
-        FileReader fr = null ;
-        char[] cbuf = new char[512] ;
-        int size = 0 ;
-        String ret = "";
-        try {
-            // open file.
-            fr = new FileReader(file) ;
-
-            // read file.
-            while ((size = fr.read(cbuf)) != -1) {
-                // TODO : use data
-                for (int i=0; i<size; i++) {
-                    ret += cbuf[i];
-                }
-            }
-
-            fr.close() ;
-        } catch (Exception e) {
-            e.printStackTrace() ;
-        }
-        return ret;
+    public int getYear() {
+        return year;
     }
 
+    public int setYear(int year) {
+        if(year <= Calendar.DAY_OF_YEAR-2) {
+            Toast.makeText(context, "2년이상 지난 날의 일기는 작성,수정 할 수 없습니다.", Toast.LENGTH_SHORT).show();
+            return 1;
+        }
+        if (Calendar.DAY_OF_YEAR < year){
+            Toast.makeText(context, "미래일기는 지원하지 않습니다.", Toast.LENGTH_SHORT).show();
+            return 1;
+        }
+        this.year = year;
+        return 0;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int setMonth(int month) {
+        if((month < 1) || (12 < month)) {
+            Toast.makeText(context, "월을 똑바로 입력하세요.", Toast.LENGTH_SHORT).show();
+            return 1;
+        }
+        this.month = month;
+        return 0;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int setDay(int day) {
+        if((day < 1) || (31 < day)) {
+            Toast.makeText(context,"일을 똑바로 입력하세요.",Toast.LENGTH_SHORT).show();
+            return 1;
+        }
+        if(day==31) {
+            switch (Calendar.DAY_OF_MONTH) {
+                case 2: case 4: case 6: case 9: case 11:
+                    Toast.makeText(context, "이 달에 31일은 없 습 니 다 .",Toast.LENGTH_SHORT).show();
+            }
+        }
+        this.day = day;
+        return 0;
+    }
+
+    public String getDiaryText() {
+        return diaryText;
+    }
+
+    public void setDiaryText(String diaryText) {
+        this.diaryText = diaryText;
+    }
 }
--- app/src/main/java/com/apress/gerber/diary_proj/NormalDiaryActivity.java
+++ app/src/main/java/com/apress/gerber/diary_proj/NormalDiaryActivity.java
@@ -15,7 +15,9 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.Calendar;
 
 /**
  * Created by Arduino on 2017-05-30.
@@ -24,6 +26,7 @@ import java.io.FileWriter;
 public class NormalDiaryActivity extends AppCompatActivity {
 
 
+    //Api23부터 permission함수와 ask함수가 자바파일에도 있어야  외부메모리에 파일쓰기 권한을 가질 수있다.
     protected boolean shouldAskPermissions() {
         return (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1);
     }
@@ -37,18 +40,47 @@ public class NormalDiaryActivity extends AppCompatActivity {
         int requestCode = 200;
         requestPermissions(permissions, requestCode);
     }
+
+
+
     protected void onCreate(Bundle bundle){
         super.onCreate(bundle);
         setContentView(R.layout.activity_normal_diary);
-
         final EditText diaryEditText = (EditText) findViewById(R.id.diaryEditText);
         Button saveButton = (Button) findViewById(R.id.saveButton);
-        saveButton.setOnClickListener(new View.OnClickListener(){
+
+
+        Button dateButton = (Button) findViewById(R.id.dateButton);
+
+        dateButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View V){
+                EditText yearText = (EditText) findViewById(R.id.yearText);
+                EditText monthText = (EditText) findViewById(R.id.monthText);
+                EditText dayText = (EditText) findViewById(R.id.dayText);
+                yearText.setText(Integer.toString(Calendar.DAY_OF_YEAR));
+                monthText.setText(Integer.toString(Calendar.DAY_OF_MONTH));
+                dayText.setText(Integer.toString(Calendar.DATE));
+            }
+        });
+
+
+        saveButton.setOnClickListener(new View.OnClickListener(){   //저장버튼 눌렀을 시 동작부
             @Override
             public void onClick(View v){
+                EditText yearText = (EditText) findViewById(R.id.yearText);
+                EditText monthText = (EditText) findViewById(R.id.monthText);
+                EditText dayText = (EditText) findViewById(R.id.dayText);
                 Toast.makeText(getApplicationContext(),"저장이 되려나...",Toast.LENGTH_SHORT).show();
-                File file = new File("/storage/emulated/0/text.txt");
-                String txt = diaryEditText.getText().toString();
+                Diary diary = new Diary(getApplicationContext(), diaryEditText.getText().toString());
+                diary.setYear(Integer.parseInt(yearText.getText().toString()));
+                diary.setMonth(Integer.parseInt(monthText.getText().toString()));
+                diary.setDay(Integer.parseInt(dayText.getText().toString()));
+                File file = new File("/storage/emulated/0/.Diaries/" + yearText.getText().toString() +
+                                                                      "/" + monthText.getText().toString() +
+                                                                      "/" + dayText.getText().toString() +
+                                                                      "/" + "일기제목"+".txt");
+
                 FileWriter fw = null;
                 BufferedWriter bufw = null;
                 if(shouldAskPermissions()){
@@ -57,7 +89,7 @@ public class NormalDiaryActivity extends AppCompatActivity {
                 try {
                     fw = new FileWriter(file);
                     bufw = new BufferedWriter(fw);
-                    bufw.write(txt);
+                    bufw.write(diary.getDiaryText());
                     Toast.makeText(getApplicationContext(),"저장완료!",Toast.LENGTH_SHORT).show();
                 }catch(Exception ex){
                     Toast.makeText(getApplicationContext(),"File Error : "+ex.toString(),Toast.LENGTH_SHORT).show();
@@ -70,14 +102,11 @@ public class NormalDiaryActivity extends AppCompatActivity {
                 }
 
 
-
-                /*                Intent intent = new Intent(getApplicationContext(), NormalDiaryViewActivity.class);
-                intent.putExtra("normalDiaryView", txt);
-                startActivity(intent);
-                */
-
-
             }
-        });
+        }); //저장버튼 동작부 끝
     }
 }
+
+
+
+
