8996d80189f3255f9010121c6083830cbcec0fcb
dlcskgy <hyo517@naver.com>
2017-06-09 00:36:37 +0900

    목록이 나와야하는 액티비티는 뜨는데 목록이 안뜸 ㅜㅜ

--- app/src/main/java/com/apress/gerber/diary_proj/DiaryListActivity.java
+++ app/src/main/java/com/apress/gerber/diary_proj/DiaryListActivity.java
@@ -1,5 +1,6 @@
 package com.apress.gerber.diary_proj;
 
+import android.app.Activity;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -19,20 +20,18 @@ import java.util.List;
  * Created by Arduino on 2017-06-05.
  */
 
-public class DiaryListActivity extends ListActivity{
+public class DiaryListActivity extends Activity {
 
-    //파일의 경로를 가져올 파일객체
-    private File diary;
 
     //월을 나타내는 디렉토리 목록을 저장할 문자열 배열
-    private ArrayList<String> monthList;
+    private ArrayList<String> monthList = new ArrayList<String>();
 
     //년도를 나타낼 디렉토리 목록을 저장할 문자열 배열
-    private ArrayList<String> yearList;
+    private ArrayList<String> yearList = new ArrayList<String>();
     private int year;
 
     //일기 리스트를 저장할 문자열 배열
-    private ArrayList<String> diaryList;
+    private ArrayList<String> diaryList = new ArrayList<String>();
 
 
 
@@ -46,15 +45,15 @@ public class DiaryListActivity extends ListActivity{
 
         final ListView list = (ListView) findViewById(R.id.diaryList);
 
-        File diaryDir = new File("/storage/emulated/0/diary");
+        File diaryDir = new File("/storage/emulated/0/Diaries");
 
         File temp[] = diaryDir.listFiles();
 
-        for(File file : temp){
-            yearList.add(file.getName());
+        for(int i = 0; i < temp.length; i++){
+            this.yearList.add(temp[i].getName());
         }
         //year spinner 설정
-        ArrayAdapter yearAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_spinner_item, yearList);
+        ArrayAdapter yearAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_spinner_item, this.yearList);
         yearAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
         yearSpinner.setAdapter(yearAdapter);
 
@@ -64,7 +63,7 @@ public class DiaryListActivity extends ListActivity{
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id){
                 monthList.clear();  //기존에있던 monthList를 비워준다
                 year = Integer.parseInt(yearList.get(position));
-                File yeardir = new File("/storage/emulated/0/diary/" + yearList.get(position));
+                File yeardir = new File("/storage/emulated/0/Diaries/" + yearList.get(position));
 
                 File temp[] = yeardir.listFiles();
                 for(File file:temp){
@@ -86,13 +85,16 @@ public class DiaryListActivity extends ListActivity{
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id){
                 diaryList.clear();
-                File monthDir = new File("/storage/emulated/0/diary/" + year + "/" + diaryList.get(position));
+                File monthDir = new File("/storage/emulated/0/Diaries/" + year + "/" + monthList.get(position));
                 File temp[] = monthDir.listFiles();
                 for(File file:temp){
                     diaryList.add(file.getName());
                 }
                 ArrayAdapter diaryAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, diaryList);
                 list.setAdapter(diaryAdapter);
+
+
+
             }
             @Override
             public void onNothingSelected(AdapterView<?> parent){
