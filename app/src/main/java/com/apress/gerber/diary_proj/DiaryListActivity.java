e7cd7538dacbf13542ef807a1e12e9db53d014f0
dlcskgy <hyo517@naver.com>
2017-06-08 22:01:08 +0900

    diaryList를 들어가면 팅긴다 ㅜㅜ

--- app/src/main/java/com/apress/gerber/diary_proj/DiaryListActivity.java
+++ app/src/main/java/com/apress/gerber/diary_proj/DiaryListActivity.java
@@ -105,6 +105,5 @@ public class DiaryListActivity extends ListActivity{
 
 
 
-
     }
 }
--- app/src/main/java/com/apress/gerber/diary_proj/MainActivity.java
+++ app/src/main/java/com/apress/gerber/diary_proj/MainActivity.java
@@ -19,6 +19,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button diaryListButton = (Button) findViewById(R.id.diaryListButton);
+        diaryListButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(),DiaryListActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
         Button normalDiaryButton = (Button) findViewById(R.id.normalDiaryButton);
         normalDiaryButton.setOnClickListener(new View.OnClickListener(){
